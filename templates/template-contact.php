e0b33704d75e0028442beda0a77e3fcb7032d278
sosssolasticot <solene.gilloppe@orange.fr>
2022-12-02 08:59:16 -0500

    zindex

---
+++ templates/template-contact.php
@@ -0,0 +1,17 @@
+<?php /* Template Name: Gabarit Contact */?>
+
+<?php get_header(); ?>
+
+<h1> Nous joindre </h1>
+
+<section> 
+    <h2> Coordonnées </h2>
+
+</section>
+
+<section> 
+    <h2> Formulaire de contact </h2>
+    
+</section>
+
+<?php get_footer(); ?>
