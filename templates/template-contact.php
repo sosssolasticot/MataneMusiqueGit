4cecee063080a58fd204f5004ec0dc66c1a5615a
sosssolasticot <solene.gilloppe@orange.fr>
2022-12-02 09:20:23 -0500

    coordonnees

--- templates/template-contact.php
+++ templates/template-contact.php
@@ -14,20 +14,16 @@
             );
             $query = new WP_Query($args);
         ?>
-        <?php if ($query->have_posts()): ?>
-            <?php while ($query->have_posts()): $query->the_post(); ?>
-                <div class="un-service">
-                    <?php if($query -> have_posts()): $query->the_post(); ?>
-                        <?php if(has_post_thumbnail()) : ?> <!--Si le post à une image-->
-                            <div class="une-coordonnee">
-                                <?php the_post_thumbnail();?> <!--Affiche l'image-->
-                                <div class="coordonnee-texte"> <?php the_content(); ?> </div> <!--Affiche le contenu-->
-                            </div>
-                        <?php endif; ?>   
-                    <?php endif; ?>  
-                </div>
-            <?php endwhile; ?>
-        <?php endif; ?> 
+        <div class="des-coordonnees">
+            <?php if($query -> have_posts()): $query->the_post(); ?>
+                <?php if(has_post_thumbnail()) : ?> <!--Si le post à une image-->
+                    <div class="une-coordonnee">
+                        <?php the_post_thumbnail();?> <!--Affiche l'image-->
+                        <div class="coordonnee-texte"> <?php the_content(); ?> </div> <!--Affiche le contenu-->
+                    </div>
+                <?php endif; ?>   
+            <?php endif; ?>  
+        </div>
     </div>
     <div class="formulaire" >
         <h2> Formulaire de contact </h2>
